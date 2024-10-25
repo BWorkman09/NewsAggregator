d7af29ec2cec1a444a540281f44f4d2ea21165a2
BWorkman09 <bwworkma@uark.edu>
2024-10-25 17:53:55 -0500

    Adding in new load data
    Adding in new load data base data file.

--- utility/data/load_data.py
+++ utility/data/load_data.py
@@ -1,1 +1,14 @@
+import sqlite3
 
+with open(r"utility\data\News_Aggregator.db.sql", 'r', encoding="utf8") as sql_file:
+    sql_script = sql_file.read()
+
+db = sqlite3.connect('Test.db')
+cursor = db.cursor()
+cursor.executescript(sql_script)
+#db.commit()
+#db.close()
+result = cursor.execute('SELECT * from Article LIMIT 10')
+for row in result:
+    print(row)
+db.close()
